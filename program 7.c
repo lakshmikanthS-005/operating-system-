2ed8ea58cfcfa625890b48316eeaeb65dd666a4a
lakshmikanth s <112749374+lakshmikanthS-005@users.noreply.github.com>
2022-10-07 11:21:14 +0530

    Update program 7.c

--- program 7.c
+++ program 7.c
@@ -1,81 +1,48 @@
-#include <stdio.h>
-int main()
-{
-	
-
-	int n, m, i, j, k;
-	n = 5; 
-	m = 3; 
-	int alloc[5][3] = { { 0, 1, 0 }, 
-						{ 2, 0, 0 }, 
-						{ 3, 0, 2 }, 
-						{ 2, 1, 1 }, 
-						{ 0, 0, 2 } }; 
-
-	int max[5][3] = { { 7, 5, 3 }, 
-					{ 3, 2, 2 },
-					{ 9, 0, 2 },
-					{ 2, 2, 2 },
-					{ 4, 3, 3 } }; 
-
-	int avail[3] = { 3, 3, 2 }; // Available Resources
-
-	int f[n], ans[n], ind = 0;
-	for (k = 0; k < n; k++) {
-		f[k] = 0;
-	}
-	int need[n][m];
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++)
-			need[i][j] = max[i][j] - alloc[i][j];
-	}
-	int y = 0;
-	for (k = 0; k < 5; k++) {
-		for (i = 0; i < n; i++) {
-			if (f[i] == 0) {
-
-				int flag = 0;
-				for (j = 0; j < m; j++) {
-					if (need[i][j] > avail[j]){
-						flag = 1;
-						break;
-					}
-				}
-
-				if (flag == 0) {
-					ans[ind++] = i;
-					for (y = 0; y < m; y++)
-						avail[y] += alloc[i][y];
-					f[i] = 1;
-				}
-			}
-		}
-	}
-
-	int flag = 1;
-	
-	for(int i=0;i<n;i++)
-	{
-	if(f[i]==0)
-	{
-		flag=0;
-		printf("The following system is not safe");
-		break;
-	}
-	}
-	
-	if(flag==1)
-	{
-	printf("Following is the SAFE Sequence\n");
-	for (i = 0; i < n - 1; i++)
-		printf(" P%d ->", ans[i]);
-	printf(" P%d", ans[n - 1]);
-	}
-	
-
-	return (0);
-
+#include <stdio.h> 
+int main() 
+{ 
+int referenceString[10], pageFaults = 0, m, n, s, pages, frames; 
+printf("\nEnter the number of Pages:\t"); 
+scanf("%d", &pages); 
+printf("\nEnter reference string values:\n");for( m = 0; m < pages; m++) 
+{ 
+printf("Value No. [%d]:\t", m + 1); 
+scanf("%d", &referenceString[m]); 
+} 
+printf("\n What are the total number of frames:\t"); 
+{ 
+scanf("%d", &frames); 
+} 
+int temp[frames]; 
+for(m = 0; m < frames; m++) 
+{ 
+temp[m] = -1; 
+} 
+for(m = 0; m < pages; m++) 
+{ 
+s = 0; 
+for(n = 0; n < frames; n++) 
+{ 
+if(referenceString[m] == temp[n]) 
+{ 
+s++; 
+pageFaults--; 
+}} 
+pageFaults++; 
+if((pageFaults <= frames) && (s == 0)) 
+{ 
+temp[m] = referenceString[m]; 
+} 
+else if(s == 0) 
+{ 
+temp[(pageFaults - 1) % frames] = referenceString[m]; 
+} 
+printf("\n"); 
+for(n = 0; n < frames; n++) 
+{ 
+printf("%d\t", temp[n]); 
+} 
+} 
+printf("\nTotal Page Faults:\t%d\n", pageFaults); 
+return 0; 
 }
-output:
-Following is the SAFE Sequence
- P1 -> P3 -> P4 -> P0 -> P2
